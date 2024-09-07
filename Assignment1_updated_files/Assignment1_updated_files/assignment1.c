d6adf237bfb57a86d526b9eb7ee41fb24ab9db74
riptideradiant <al096567@ucf.edu>
2024-09-07 21:44:40 +0000

    Fixed outputs and added calculatePerimeter

--- Assignment1_updated_files/Assignment1_updated_files/assignment1.c
+++ Assignment1_updated_files/Assignment1_updated_files/assignment1.c
@@ -1,34 +1,66 @@
 #include <stdio.h>
 #include <math.h>
+#define pi 3.14159
 
-double calculateDistance(double x1val, double x2val, double y1val, double y2val)
+double askForUserInput()
 {
 
-double distanceX = x1val - x2val;
-double distanceY = y1val - y2val;
+double pointvalue = 0;
+scanf("%lf", &pointvalue);
+
+return pointvalue;
+}
+
+double distanceCalculator()
+{
+   
+printf("Enter the x value point 1: \n");
+double point1Xvalue = askForUserInput();
+
+printf("Enter the y value of point 1: \n");
+double point1Yvalue = askForUserInput();
+
+printf("Enter the x value point 2: \n");
+double point2Xvalue = askForUserInput();
+
+printf("Enter the y value point 2: \n");
+double point2Yvalue = askForUserInput();
+
+double distanceX = point1Xvalue - point2Xvalue;
+double distanceY = point1Yvalue - point2Yvalue;
 double distance = sqrt(pow(distanceX, 2) + pow(distanceY,2));
+printf("Point #1 entered: x1 = %lf; y1 = %lf \n", point1Xvalue, point1Yvalue);
+printf("Point #2 entered: x1 = %lf; y1 = %lf \n", point2Xvalue, point2Yvalue);
 
 return distance;
+}
+
+double calculateDistance()
+{
+
+double distance = distanceCalculator();
+
+printf("The distance between the two points is %lf \n", distance);
+
+return distance;
+}
+
+double calculatePerimeter()
+{
+
+double diameter = distanceCalculator();
+double perimeter = pi*diameter;
+double difficulty1 = 5;
+
+printf("The perimeter of the city encompassed by your request is %lf \n", perimeter);
+return difficulty1;
 }
 
 int main(int argc, char **argv)
 {
 
-double point1Xvalue = 0;
-double point1Yvalue = 0;
-double point2Xvalue = 0;
-double point2Yvalue = 0;
-
-printf("Enter the x then the y value of point 1: \n");
-scanf("%lf", &point1Xvalue);
-scanf("%lf", &point1Yvalue);
-
-printf("Enter the x then the y value of point 2: \n");
-scanf("%lf", &point2Xvalue);
-scanf("%lf", &point2Yvalue);
-
-double newDistance = calculateDistance(point1Xvalue, point2Xvalue, point1Yvalue, point2Yvalue);
-printf("The distance between the points is: %lf \n", newDistance);
+calculateDistance();
+calculatePerimeter();
 
 return 0;
 }
